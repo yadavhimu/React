f7744c82ca479f455031411db015cb2ceae690a3
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-03-17 21:44:22 +0530

    react examples

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -35,7 +35,11 @@ function App() {
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "skyblue"}}
           >SkyBlue</button>
-          
+          <button
+          onClick={() => setColor("purple")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "purple"}}
+          >Purple</button>
         </div>
       </div>
     </div>
